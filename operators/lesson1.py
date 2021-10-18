4e0751f12eff499d0c119c4cbfc9ef6d41766b38
Jeremyas Cornelis <74807111+jeremyascornelis@users.noreply.github.com>
2021-10-18 10:01:43 +0700

    Add files via upload

---
+++ operators/lesson1.py
@@ -0,0 +1,26 @@
+#arithmethic Operators
+"""
+1. +
+2. -
+3. *
+4. /
+5. %
+6. **
+7. //
+"""
+
+addition = 10 + 3
+substraction = 10 - 3
+multiply = 10 * 3
+floatDivision = 10 / 3
+modulo = 10 % 3
+powerOpr = 10 ** 3
+integerDivision = 10 // 3
+
+print(addition)
+print(substraction)
+print(multiply)
+print(floatDivision) #get float type as a result
+print(modulo)
+print(powerOpr)
+print(integerDivision) #get integer type as a result
---
+++ operators/lesson2.py
@@ -0,0 +1,35 @@
+#asssignment operators = assigning a value
+"""
+1. = 
+2. +=
+3. -=
+4. *=
+5. /=
+6. %=
+7. **=
+8. //=
+
+"""
+
+var1 = 10
+
+var1 += 3
+print(var1) #13
+
+var1 -= 3
+print(var1) #10
+
+var1 *= 3
+print(var1) #30
+
+var1 /= 3
+print(var1) #10.0
+
+var1 %= 3
+print(var1) #1.0
+
+var1 **= 3
+print(var1) #1.0
+
+var1 //= 3
+print(var1) #0.0
